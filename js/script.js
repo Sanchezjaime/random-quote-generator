bf4f55f7f5a83d56a97d6b322a3114c0f828459b
Jaime Sanchez <40275727+Sanchezjaime@users.noreply.github.com>
2018-10-28 21:35:39 -0700

    added background color randomizer and quote timer
    background color changes rondomly when new quote is generated and a timer is set to print new quotes.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-// FSJS - Random Quote Generator
+// Random Quote Generator
 
 // global quote objects with source, citation and year.
 var quotes = [{ quote: "Have no fear of perfection, you'll never reach it.",
-              source: "Salvador Dali"
+              source: "Salvador Dali",
+
             },
               { quote: "El que lee mucho y anda mucho, ve mucho y sabe mucho.",
-              source: "Miguel de Cervantes"
+              source: "Miguel de Cervantes",
+
             },
               { quote: "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
-              source: "Malcom X"
+              source: "Malcom X",
+              tags: "Inspirational"
             },
               { quote: "That's what I do: I drink and I know things.",
               source: "Tyrion Lannister",
-              citation: "Game of Thrones TV series"
+              citation: "Game of Thrones TV series",
+              tags: "Funny"
             },
               { quote: "And so, my fellow Americans: ask not what your country can do for you - ask what can you do for your country.",
               source: "John F. Kennedy",
               citation: "Inaugaration Address",
               year: "1961"
 }];
+
 
 
 // function generates random number and assigns it to the array index returns object from array
@@ -29,8 +34,16 @@ function getRandomQuote(array) {
 
 }
 
+//sets a random css rgb color value to the background-color
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    document.body.style.backgroundColor = backgroundColor;
 
 
+}
 
 // function prints quote
 function printQuote() {
@@ -43,12 +56,24 @@ function printQuote() {
   if (randomQuote.year) {
     quoteString += '<span class="year">' + randomQuote.year + '</span></p>'
   }
+  if (randomQuote.tags) {
+    quoteString += '<h3 class="tags"> ' + randomQuote.tags + '</h3>'
+  }
   document.getElementById("quote-box").innerHTML = quoteString;
+  //changes the background color when printquote function gets called
+  randomColor();
+
 
 }
 
 
+//changes quote automatically every three seconds
+var startTimer = window.setInterval(printQuote, 3000);
+//stops the quote from automatically changing after 50 seconds
+setTimeout(function() { clearInterval(startTimer); }, 50000);
+
 printQuote();
+
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
